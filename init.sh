f62f5f56c09bd20862745a22e28b334bf64bfb14
AlexFireKazak <alexfirekazak@gmail.com>
2022-05-17 04:00:55 +0300

    failed 4 test

--- init.sh
+++ init.sh
@@ -3,6 +3,3 @@ sudo rm /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
