0a165ea2a7cad5008375fdef37dab161e0c13a7d
AlexFireKazak <alexfirekazak@gmail.com>
2022-05-20 22:25:38 +0300

    correction regular expressions in ask/urls.py: path -> url

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'qa/', include('qa.urls')),
-    url('admin/', 'admin.site.urls'),
+    url(r'^qa/', include('qa.urls')),
+    url('^admin/', 'admin.site.urls'),
 ]
