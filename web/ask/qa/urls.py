29997d0a0f45e7dbcd8f1618c2812ba1de19d700
AlexFireKazak <alexfirekazak@gmail.com>
2022-05-20 22:12:45 +0300

    correction regular expressions in qa/urls.py: path -> url

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -1,13 +1,14 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.test),
-    path(r'^login/.*$', views.test),
-    path(r'^signup/.*$', views.test),
-    path(r'^question/\/d+/$', views.test),
-    path(r'^ask/.*$', views.test),
-    path(r'^popular/>*$', views.test),
-    path(r'^new/>*$', views.test),
+    url(r'^$', 'views.test'),
+    url(r'^login/.*$', 'views.test'),
+    url(r'^signup/.*$', 'views.test'),
+    url(r'^question/\/d+/$', 'views.test'),
+    url(r'^ask/.*$', 'views.test'),
+    url(r'^popular/>*$', 'views.test'),
+    url(r'^new/>*$', 'views.test'),
 ]
