7f2f6c8f58957c4139a6e78c98f312145c73e32d
AlexFireKazak <alexfirekazak@gmail.com>
2022-05-20 21:30:42 +0300

    correction init.sh and nginx.conf

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
 sudo /etc/init.d/nginx restart
 gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app &
+gunicorn -w 2 -c /home/box/web/etc/gunicorn_django_conf.py ask.wsgi:application &
