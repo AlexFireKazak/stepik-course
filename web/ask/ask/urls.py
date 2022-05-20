c57eae64fa8f1b2f5c00a08c62de7add4ad0abc3
AlexFireKazak <alexfirekazak@gmail.com>
2022-05-20 23:06:37 +0300

    new try with https://github.com/Padking/web/tree/372473eafad3d3a9cd9574cdd91a3e5c76315c0f

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -13,17 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import url, include
 
-urlpatterns = (
-    url(r'^qa/', include('qa.urls')),
-    url(r'^login/', include('qa.urls')),
-    url(r'^$', include('qa.urls')),
-    url(r'^signup/.*$', include('qa.urls')),
-    url(r'^question/\/d+/$', include('qa.urls')),
-    url(r'^ask/.*$', include('qa.urls')),
-    url(r'^popular/>*$', include('qa.urls')),
-    url(r'^new/>*$', include('qa.urls')),
+from qa import views
+
+
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
-)
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/\d+/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
+]
