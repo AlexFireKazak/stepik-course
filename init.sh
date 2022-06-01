342d97e566476a20c805c543d7975db9ffd6b38c
AlexFireKazak <alexfirekazak@gmail.com>
2022-06-01 16:29:20 +0300

    try 2.3.11 with use https://github.com/Cheater115/stepic_web_project/ in this commit update init.sh for mysql start, update models.py and settings.py

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+$DB_NAME="ask"
+DB_USER_NAME="box"
 mv web /home/box/
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-available/default
@@ -6,3 +8,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
 sudo /etc/init.d/nginx restart
 gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app &
 gunicorn -w 2 -c /home/box/web/etc/gunicorn_django_conf.py ask.wsgi:application &
+sudo rm /etc/mysql/conf.d/mysql.cnf
+sudo /etc/init.d/mysql start
+sudo mysql -u root -e "create database if not exists $DB_NAME;"
+sudo mysql -u root -e "grant all privileges on $DB_NAME.* to '$DB_USER_NAME'@'localhost' with grant option;"
