570a142df05664657e97b5e2927e3e37f9007c0d
AlexFireKazak <alexfirekazak@gmail.com>
2022-06-01 16:43:01 +0300

    try 2.3.11 with use https://github.com/Cheater115/stepic_web_project/ in this commit correct init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-$DB_NAME="ask"
+DB_NAME="ask"
 DB_USER_NAME="box"
+DJ_APP_NAME="qa"
+
 mv web /home/box/
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-available/default
@@ -8,7 +10,13 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
 sudo /etc/init.d/nginx restart
 gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app &
 gunicorn -w 2 -c /home/box/web/etc/gunicorn_django_conf.py ask.wsgi:application &
+# SQL Parth
 sudo rm /etc/mysql/conf.d/mysql.cnf
 sudo /etc/init.d/mysql start
 sudo mysql -u root -e "create database if not exists $DB_NAME;"
 sudo mysql -u root -e "grant all privileges on $DB_NAME.* to '$DB_USER_NAME'@'localhost' with grant option;"
+#put my setup
+sudo ln -sf /home/box/web/etc/mysql.cnf /etc/mysql/conf.d/mysql.cnf
+sudo /etc/init.d/mysql restart
+/home/box/web/ask/manage.py makemigrations $DJ_APP_NAME
+/home/box/web/ask/manage.py migrate
