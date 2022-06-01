303ebd546cab05b14969833191f41c31ed592ca3
AlexFireKazak <alexfirekazak@gmail.com>
2022-06-01 20:29:50 +0300

    try 2.3.11 with use comments

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+#import pymysql
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +87,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'web',
         'USER': 'box',
-        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '',
         'OPTIONS': {
