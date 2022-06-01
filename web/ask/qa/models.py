1946d38fa9b988bc725fc5538c9898ab6f2b1530
AlexFireKazak <alexfirekazak@gmail.com>
2022-06-01 21:25:26 +0300

    try 2.3.11 with github all copy

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -6,33 +6,32 @@ from django.urls import reverse
 
 # Own manager for models Question
 class QuestionManager(models.Manager):
-    def new(self):
+	def new(self):
         return self.order_by('-added_at')
     def popular(self):
         return self.order_by('-rating')
-
 # Model Question
 class Question(models.Model):
-    #fields
-    title = models.CharField(max_length=255)
+	# fields
+	title = models.CharField(max_length = 255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    added_at = models.DateTimeField(auto_now_add = True)
+    rating = models.IntegerField(default = 0)
     objects = QuestionManager()
     # links
-    author =models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-    #methods
+    author = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name = 'likes')
+    # methods
     def get_url(self):
-        return reverse('question', kwargs={'id':self.id})
-    def __unicode__(self):
+        return reverse('question', kwargs = {'id': self.id})
+    def __unicode__(self) :
         return self.title
 
 # Model Answer
 class Answer(models.Model):
-    #fields
+    # fields
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    #links
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add = True)
+    # links
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
